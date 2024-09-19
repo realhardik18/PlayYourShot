89d4dba1c7288dbc30ccd6045e6e7d2a749cac7f
realhardik18 <realhardik18@gmail.com>
2024-09-19 08:38:18 +0530

    changing techstack mid project :p

--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { keys } from './creds';
-// Add your Supabase API URL and Anon Key
-const supabaseUrl = keys.API_URL;
-const supabaseAnonKey = keys.API_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
